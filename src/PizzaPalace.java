999de90c296f6eccaaf5f1075f3ab9188ada48d4
Kent Collins <kent.r.collins@gmail.com>
2014-10-18 06:47:39 +0100

    Added Flowchart

---
+++ src/PizzaPalace.java
@@ -0,0 +1,26 @@
+/**
+ * Determines the number of pizza pies required to feed an after school club.
+ * 
+ * Basic program
+ * 	Prompt the user to enter the number of people to be fed, the number of
+ * 	slices allowed for each person, and the number of slices in each whole pie.
+ * 	Given these inputs, determine and display the number of pizzas to purchase.
+ * 
+ * For guidance, refer to the flowchart PizzaPalace.pdf distributed with this file
+ * 
+ * @author <change to your name>
+ * @version <today's date>
+ */
+public class PizzaPalace {
+
+	public static void main(String[] args) {
+		// TODO Replace this section with your own code
+		
+		// NOTE
+		// The Math class provides a function, ceiling(), which will take a double
+		// and return the next higher integer value.
+		// For example, Math.ceiling(4.125) would evaluate to 5
+
+	}
+
+}
