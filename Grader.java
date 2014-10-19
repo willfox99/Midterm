20c2f7e4b2de93e6ef2e397841cff566c8b8b9bf
Kent Collins <kent.r.collins@gmail.com>
2014-10-19 21:04:40 +0100

    Added instructions

--- Grader.java
+++ Grader.java
@@ -1,3 +1,23 @@
 /**
+*  Calculates the grade a student receives on an examination
 *
+*  The program first prompts the user to enter the number of points possible
+*  on the exam.  Then, the program prompts for the number of items missed; if a positive
+*  number is entered, the program responds by calculating and displaying the percentage
+*  score out of 100.  If a negative number is entered, the program prints a "Thank You" message
+*  and exits.
+*  
+*  A sample interaction might be: (user input marked with '>')
+*  "Enter the number of items on the exam"
+*  >25
+*  "Enter the number of items missed"
+*  >3.5
+*  "Missing 3.5 results in a score of 86.0"
+*  "Enter the number of items missed"
+*  >0
+*  "Missing 0 results in a score of 100.0"
+*  "Enter the number of items missed"
+*  >-100
+*  "Thank you -- have a great day!"
+*  
 */
