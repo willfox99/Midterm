1b6cee07bcf5470e634509d00b0d9750a2debaa0
willfox99 <william_fox@asl.org>
2014-10-20 12:40:25 +0100

    Finished both projects

--- PizzaPalace.java
+++ PizzaPalace.java
@@ -1,26 +1,32 @@
-/**
- * Determines the number of pizza pies required to feed an after school club.
- * 
- * Basic program
- * 	Prompt the user to enter the number of people to be fed, the number of
- * 	slices allowed for each person, and the number of slices in each whole pie.
- * 	Given these inputs, determine and display the number of pizzas to purchase.
- * 
- * For guidance, refer to the flowchart PizzaPalace.pdf distributed with this file
- * 
- * @author <change to your name>
- * @version <today's date>
- */
+import java.util.Scanner;
 public class PizzaPalace {
-
-	public static void main(String[] args) {
-		// TODO Replace this section with your own code
-		
-		// NOTE
-		// The Math class provides a function, ceiling(), which will take a double
-		// and return the next higher integer value.
-		// For example, Math.ceiling(4.125) would evaluate to 5
-
-	}
-
+    
+    
+    public static void main(String[] args) {
+        
+        double numPeople = 0.0;
+        double numSlices = 0.0;
+        int slicesPerPie = 0;
+        double slicesNeeded = 0.0;
+        
+        
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("How many people are there? ");
+        numPeople = scnr.nextDouble();
+        System.out.println("How many slices per person? ");
+        numSlices = scnr.nextDouble();
+        System.out.println("How many slices in a pizza? ");
+        slicesPerPie = scnr.nextInt();
+        
+        slicesNeeded = numPeople * numSlices;
+        slicesNeeded = slicesNeeded / slicesPerPie;
+        
+        System.out.println("You will need " + Math.ceil(slicesNeeded) + " pizzas!");
+        
+        
+        
+    }
+    
 }
+
+
